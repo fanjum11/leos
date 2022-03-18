b8a2285ba6dffce9f04a381ec90e2448d55ae0ed
fanjum <fanjum@groguru.com>
2022-03-18 16:31:06 -0230

    adding test case

--- tests/create_eval_earn_test.ts
+++ tests/create_eval_earn_test.ts
@@ -3,24 +3,67 @@ import { Clarinet, Tx, Chain, Account, types } from 'https://deno.land/x/clarine
 import { assertEquals } from 'https://deno.land/std@0.90.0/testing/asserts.ts';
 
 Clarinet.test({
-    name: "Ensure that <...>",
+    name: "Ensure that any creator can buy edu tokens",
     async fn(chain: Chain, accounts: Map<string, Account>) {
-        let block = chain.mineBlock([
-            /* 
-             * Add transactions with: 
-             * Tx.contractCall(...)
-            */
-        ]);
-        assertEquals(block.receipts.length, 0);
-        assertEquals(block.height, 2);
+      var deployer = accounts.get("deployer")!;
+      var member = accounts.get("deployer")!;
+      var organisation = accounts.get("wallet_2")!;
+      var tokenTrait = `${deployer.address}.edu-token`;
+  
+      let block = chain.mineBlock([
+        Tx.contractCall(
+          "create_eval_earn",
+          "purchase_edu_token",
+          [types.principal(tokenTrait), types.uint(1000)],
+          deployer.address
+        ),
+      ]);
+  
+      //chain.mineEmptyBlock(10);
+    
+      //block.receipts[0].result.expectOk().expectUint(1);
+      block.receipts[0].result.expectOk().expectBool(true);
+      assertEquals("(ok true)", block.receipts[0].result);
+    },
+  });
 
-        block = chain.mineBlock([
-            /* 
-             * Add transactions with: 
-             * Tx.contractCall(...)
-            */
-        ]);
-        assertEquals(block.receipts.length, 0);
-        assertEquals(block.height, 3);
+Clarinet.test({
+    name: "Ensure that the any creator can create a test by paying edu tokens",
+    async fn(chain: Chain, accounts: Map<string, Account>) {
+        var notContractOwner = accounts.get("wallet_1")!;
+        var deployer = accounts.get("deployer")!;
+        var member = accounts.get("deployer")!;
+        var tokenTrait = `${deployer.address}.edu-token`;
+
+        let block = chain.mineBlock([
+            Tx.contractCall(
+                "create_eval_earn",
+                "purchase_edu_token",
+                [types.principal(tokenTrait), types.uint(1000)],
+                deployer.address
+              ),
+            Tx.contractCall(
+                "create_eval_earn",
+                "test_init",
+                [
+                    types.principal(tokenTrait),
+                    types.uint(10),
+                    types.uint(8),
+                    types.uint(15),
+                    types.uint(100),
+                    '0xabcd',
+                    types.utf8("abcdefgh"),
+                    types.utf8("acbdacbd")
+                ],
+                deployer.address
+            ),
+                ]);
+        assertEquals(block.receipts.length, 1);
+        assertEquals(block.height, 2);
+        block.receipts[0].result.expectOk().expectBool(true);
+        //block.receipts[1].result.expectOk().expectUint(1);
     },
 });
+
+
+
--- tests/edu-token_test.ts
+++ tests/edu-token_test.ts
@@ -1,26 +1,33 @@
 
 import { Clarinet, Tx, Chain, Account, types } from 'https://deno.land/x/clarinet@v0.14.0/index.ts';
 import { assertEquals } from 'https://deno.land/std@0.90.0/testing/asserts.ts';
-
-Clarinet.test({
-    name: "Ensure that <...>",
+  
+  Clarinet.test({
+    name: "Ensure that transfer can only be called by token owner",
     async fn(chain: Chain, accounts: Map<string, Account>) {
-        let block = chain.mineBlock([
-            /* 
-             * Add transactions with: 
-             * Tx.contractCall(...)
-            */
-        ]);
-        assertEquals(block.receipts.length, 0);
-        assertEquals(block.height, 2);
-
-        block = chain.mineBlock([
-            /* 
-             * Add transactions with: 
-             * Tx.contractCall(...)
-            */
-        ]);
-        assertEquals(block.receipts.length, 0);
-        assertEquals(block.height, 3);
+      var owner = accounts.get("wallet_1")!;
+      var notOwner = accounts.get("wallet_2")!;
+  
+      let block = chain.mineBlock([
+        Tx.contractCall(
+          "edu-token",
+          "mint",
+          [types.uint(10), types.principal(owner.address)],
+          owner.address
+        ),
+        Tx.contractCall(
+          "edu-token",
+          "transfer?",
+          [
+            types.uint(1),
+            types.principal(owner.address),
+            types.principal(notOwner.address),
+          ],
+          notOwner.address
+        ),
+        Tx.contractCall("edu-token", "burn", [types.uint(1)], owner.address),
+      ]);
+  
+      block.receipts[1].result.expectErr().expectUint(100);
     },
-});
+  });
